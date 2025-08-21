828f4a7af5b8eb5800f7e0a78bb4591ca73349f6
Woramin-228-5 <sukuyyo@gmail.com>
2025-08-21 09:32:07 +0700

    1

--- labsql4.sql
+++ labsql4.sql
@@ -1,1 +1,24 @@
-SELECT 
+SELECT CategoryName, ProductName, UnitPrice
+FROM Categories , Products
+WHERE Categories.CategoryID = Products.CategoryID
+ORDER BY CategoryName
+
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products join Categories
+on Products.CategoryID = Categories.CategoryID
+
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products as p,Categories as c
+where p.CategoryID = c.CategoryID 
+
+SELECT CompanyName,OrderID
+from orders, shippers
+WHERE shippers.ShipperID = orders.ShipVia
+
+SELECT CompanyName,OrderID
+from orders JOIN shippers
+on shippers.ShipperID = orders.ShipVia
+
+SELECT ProductID, ProductName, CompanyName,Country 
+from Products p, Suppliers s
+WHERE p.SupplierID = s.SupplierID
